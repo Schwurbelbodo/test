bc1983029d8e40a4a9eabf3ea02f304eeb9e7a9b
Schwurbelbodo <105820296+Schwurbelbodo@users.noreply.github.com>
2022-05-18 23:48:04 +0200

    Update karte.py

--- karte.py
+++ karte.py
@@ -3,12 +3,12 @@ import streamlit as st
 from folium.plugins import Draw
 from streamlit_folium import st_folium
 
-def app():
-    st.set_page_config(layout="wide")
 
-    "## Test"
+st.set_page_config(layout="wide")
 
-    m = folium.Map(location=[39.949610, -75.150282], zoom_start=5)
-    Draw(export=True).add_to(m)
-    output = st_folium(m, width=700, height=500)
+"## Test"
+
+m = folium.Map(location=[39.949610, -75.150282], zoom_start=5)
+Draw(export=True).add_to(m)
+output = st_folium(m, width=700, height=500)
     
